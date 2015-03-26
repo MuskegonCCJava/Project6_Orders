4cf316d2b556a6e1fcd65ba4a860676cdeea367c
KyletheWolff <kyle.a.wolff87@gmail.com>
2015-03-26 00:18:54 -0400

    Completed

---
+++ src/ShippingOrder.java
@@ -0,0 +1,19 @@
+
+public class ShippingOrder extends Order {
+	
+	float shippingAndhandling = 4.0f;
+
+	public ShippingOrder() {
+		
+	}
+	
+
+	@Override
+	public float computePrice() {
+		
+		totalPrice = shippingAndhandling +  (unitPrice * quantityOrdered);
+		
+		return totalPrice;
+	}
+
+}
